97f7e2a3b1bc8da48c5d4ba08729894d71508e4f
Mitchell Olsen <olsenmd@my.lanecc.edu>
2025-10-18 18:52:40 -0700

    Customer V1

---
+++ ado_lab_2023-stewartl71-master/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using MMABooksBusinessClasses;
+using MMABooksDBClasses;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        [Test]
+        public void CanCreateCustomer_DefaultConstructor()
+        {
+            var c = new Customer();
+            Assert.NotNull(c);
+        }
+
+        [Test]
+        public void CanSetProperties_WithValidation()
+        {
+            var c = new Customer
+            {
+                Name = "John Smith",
+                Address = "123 Main St",
+                City = "Eugene",
+                State = "OR",
+                ZipCode = "97401"
+            };
+            Assert.AreEqual("John Smith", c.Name);
+        }
+
+        [Test]
+        public void AddGetDeleteCustomer_RoundTrip()
+        {
+            var c = new Customer
+            {
+                Name = "Unit Test",
+                Address = "456 Pine St",
+                City = "Portland",
+                State = "OR",
+                ZipCode = "97035"
+            };
+
+            int id = CustomerDB.AddCustomer(c);
+            Assert.Greater(id, 0);
+
+            var fromDb = CustomerDB.GetCustomer(id);
+            Assert.AreEqual(c.Name, fromDb.Name);
+
+            bool deleted = CustomerDB.DeleteCustomer(fromDb);
+            Assert.IsTrue(deleted);
+        }
+    }
+}
